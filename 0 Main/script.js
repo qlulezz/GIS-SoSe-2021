cf4f946b01daafa582ce5d0037d2ff294358e3a7
Henry <HenryCraft@gmx.de>
2021-04-02 20:31:21 +0200

    Cleanup Code

--- 0 Main/script.js
+++
@@ -1,2 +0,0 @@
-console.log('JS Console Log Test');
-alert('Die Seite wird geladen, wenn auf OK geklickt wird. \n(Wenn diese Nachricht sichtbar ist, dann funktioniert JavaScript!)')
---
+++ 0_Main/script.js
@@ -0,0 +1,2 @@
+console.log('JS Console Log Test');
+alert('Die Seite wird geladen, wenn auf OK geklickt wird. \n(Wenn diese Nachricht sichtbar ist, dann funktioniert JavaScript!)')
