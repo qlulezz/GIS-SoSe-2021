33f74282f9f122ea7b8008c13e6e6a39e5475891
Henry <HenryCraft@gmx.de>
2021-04-26 09:06:33 +0200

    Added namespace

--- 2_Scripting/Aufgabe_2.1/script.ts
+++ 2_Scripting/Aufgabe_2.1/script.ts
@@ -1,18 +1,20 @@
 console.log("Hallo Welt");
 
+namespace A1 {
+
 // Aufgabe 1 - Basics
-function a1(): void {
-    let x: string = "Alles";
-    console.log(x);
-    func();
-    console.log("Logo!");
-}
+    function a1(): void {
+        let x: string = "Alles";
+        console.log(x);
+        func();
+        console.log("Logo!");
+    }
 
-a1();
+    a1();
 
-function func(): void {
-    console.log("Klar?");
-}
+    function func(): void {
+        console.log("Klar?");
+    }
 // Hinweis: Code wurde nicht 1:1 kopiert: 'func1' wurde mit 'func' ersetzt, da 'func1' in Aufgabe 3 verwendet wird.
 // 1 a)
 // Ausgabe: "Alles" "Klar?" "Logo!" 
@@ -25,32 +27,32 @@ function func(): void {
 // Die Reihenfolge habe ich in 1 a) beschrieben, der Debugger bestätigt dies.
 
 // 1 c)
-function a(): void {
-    func0();
-    console.log("Gute!");
-    func0();
-    console.log("klar?");
-    func0();
-    console.log("Logo!");
-}
+    function a(): void {
+        func0();
+        console.log("Gute!");
+        func0();
+        console.log("klar?");
+        func0();
+        console.log("Logo!");
+    }
 
-a();
+    a();
 
-function func0(): void {
-    console.log("Alles");
-}
+    function func0(): void {
+        console.log("Alles");
+    }
 
 // Aufgabe 2 - Kontinuierliche Variablenmanipulation
-function a2(): void {
-    let i: number = 9;
+    function a2(): void {
+        let i: number = 9;
 
-    do {
-        console.log(i);
-        i = i - 1;
-    } while (i > 0);
-}
+        do {
+            console.log(i);
+            i = i - 1;
+        } while (i > 0);
+    }
 
-a2();
+    a2();
 
 // Ausgabe: Zahlen herunterzählend von 9 bis 1
 //   -> 'i' ist anfänglich 9. 'do' und 'while' starten eine Schleife.
@@ -59,36 +61,36 @@ a2();
 // Aufgabe 3 - Fehler erkennen und vermeiden lernen
 // Aufgabe 3 ist schwierig nur mit Kommentaren zu lösen, daher ist hier ein nicht ganz fehlerfreier Code-Block:
 /*
-function a1(): string {
-    let x: int[] = true;
-    console.log(y)
-    func[];
-    Console.log("Logo!");
-}
+    function a1(): string {
+        let x: int[] = true;
+        console.log(y)
+        func[];
+        Console.log("Logo!");
+    }
 */
 
 // Aufgabe 4 - Gobal vs Lokal
-let x: string = "Hallo";
-console.log(x);
-func1(x);
-console.log(x);
-func2();
-func3();
-console.log(x);
-
-function func1(y: string): void {
-    y = "Bla";
-    console.log(y);
-}
-
-function func2(): void {
-    let x: string = "Blubb";
+    let x: string = "Hallo";
     console.log(x);
-}
+    func1(x);
+    console.log(x);
+    func2();
+    func3();
+    console.log(x);
 
-function func3(): void {
-    x = "Test";
-}
+    function func1(y: string): void {
+        y = "Bla";
+        console.log(y);
+    }
+
+    function func2(): void {
+        let x: string = "Blubb";
+        console.log(x);
+    }
+
+    function func3(): void {
+        x = "Test";
+    }
 
 // 4 a)
 // Ausgabe: "Hallo" "Bla" "Hallo" "Blubb" "Test"
@@ -107,131 +109,133 @@ function func3(): void {
 
 // Aufgabe 5 - Schleifen, Funktionen und andere Kontrollstrukturen
 // 5 a)
-console.log("5 a) - Multiply:");
-function multiply(x1: number, x2: number): number {
-    return x1 * x2;
-}
+    console.log("5 a) - Multiply:");
+    function multiply(x1: number, x2: number): number {
+        return x1 * x2;
+    }
 
-let x1: number = 6;
-let x2: number = 10;
-console.log(multiply(x1, x2));
+    let x1: number = 6;
+    let x2: number = 10;
+    console.log(multiply(x1, x2));
 
 // 5 b)
-console.log("5 b) - Maximale Zahl:");
-function max(x1: number, x2: number): number {
-    if (x1 > x2) {
-        return x1;
+    console.log("5 b) - Maximale Zahl:");
+    function max(x1: number, x2: number): number {
+        if (x1 > x2) {
+            return x1;
+        }
+        return x2;
     }
-    return x2;
-}
-console.log(max(x1, x2));
+    console.log(max(x1, x2));
 
 // 5 c)
-console.log("5 c) - Zahlen von 0 bis 100 addiert:");
-function count(): number {
-    let i: number = 100;
-    let o: number = 0;
-    do {
-        o += i;
-        i--;
-    } while (i > 0);
-    return o;
-}
-console.log(count());
+    console.log("5 c) - Zahlen von 0 bis 100 addiert:");
+    function count(): number {
+        let i: number = 100;
+        let o: number = 0;
+        do {
+            o += i;
+            i--;
+        } while (i > 0);
+        return o;
+    }
+    console.log(count());
 
 // 5 d)
-console.log("5 d) - 10 Random Numbers:");
-for (let i: number = 0; i < 10; i++) {
-    console.log(Math.floor(Math.random() * 100));
-}
+    console.log("5 d) - 10 Random Numbers:");
+    for (let i: number = 0; i < 10; i++) {
+        console.log(Math.floor(Math.random() * 100));
+    }
 
 // 5 e)
-console.log("5 e) - Fakultät:");
-function factorial(n: number): number {
-    if (n < 1) {
-        return 1;
-    } else {
-        let r: number = 1;
-        for (let i: number = 2; i <= n; i++) {
-            r *= i;
+    console.log("5 e) - Fakultät:");
+    function factorial(n: number): number {
+        if (n < 1) {
+            return 1;
+        } else {
+            let r: number = 1;
+            for (let i: number = 2; i <= n; i++) {
+                r *= i;
+            }
+            return r;
         }
-        return r;
     }
-}
-console.log(factorial(9));
+    console.log(factorial(9));
 
 // 5 f)
-console.log("5 f) - Schaltjahre:");
-function leapyear(): void {
-    for (let i: number = 1900; i < 2021; i++) {
-        if (i % 4 == 0 && i % 100 != 0 || i % 400 == 0) {
-            console.log(i);
+    console.log("5 f) - Schaltjahre:");
+    function leapyear(): void {
+        for (let i: number = 1900; i < 2021; i++) {
+            if (i % 4 == 0 && i % 100 != 0 || i % 400 == 0) {
+                console.log(i);
+            }
         }
     }
-}
-leapyear();
+    leapyear();
 
 // Aufgabe 6 - Mehr Schleifen und Funktionen
 // 6 a)
-console.log("6 a) - #:");
-function hash(): void {
-    let x: string = "#";
-    for (let i: number = 0; i < 7; i++) {
-        console.log(x);
-        x += "#";
+    console.log("6 a) - #:");
+    function hash(): void {
+        let x: string = "#";
+        for (let i: number = 0; i < 7; i++) {
+            console.log(x);
+            x += "#";
+        }
     }
-}
-hash();
+    hash();
 
 // 6 b)
-console.log("6 b) und c)- FizzBuzz:");
-function FizzBuzz(): void {
-    for (let i: number = 1; i < 100; i++) {
-        if (i % 15 == 0) {
-            console.log("FizzBuzz");
-        } else if (i % 3 != 0 && i % 5 == 0) {
-            console.log("Buzz");
-        } else if (i % 3 == 0) {
-            console.log("Fizz");
-        } else {
-            console.log(i);
+    console.log("6 b) und c)- FizzBuzz:");
+    function FizzBuzz(): void {
+        for (let i: number = 1; i < 100; i++) {
+            if (i % 15 == 0) {
+                console.log("FizzBuzz");
+            } else if (i % 3 != 0 && i % 5 == 0) {
+                console.log("Buzz");
+            } else if (i % 3 == 0) {
+                console.log("Fizz");
+            } else {
+                console.log(i);
+            }
         }
     }
-}
-FizzBuzz();
+    FizzBuzz();
 
 // 6 d)
-console.log("6 d) - Checkerboard:");
-function checkerboard(): void {
-    let output: string = "";
-    for (let i: number = 0; i < 4; i++) {
-        let x: string = "";
-        let y: string = "";
-        for (let j: number = 0; j < 8; j++) {
-            x += " ";
-            y += "#";
-            [x, y] = [y, x];
+    console.log("6 d) - Checkerboard:");
+    function checkerboard(): void {
+        let output: string = "";
+        for (let i: number = 0; i < 4; i++) {
+            let x: string = "";
+            let y: string = "";
+            for (let j: number = 0; j < 8; j++) {
+                x += " ";
+                y += "#";
+                [x, y] = [y, x];
+            }
+            output += x + "\n" + y + "\n";
         }
-        output += x + "\n" + y + "\n";
+        console.log(output);
     }
-    console.log(output);
-}
-checkerboard();
+    checkerboard();
 
 // 6 e)
-console.log("6 e) - Custom Checkerboard:");
-function checkerboardcustom(height: number, width: number): void {
-    let output: string = "";
-    for (let i: number = 0; i < height / 2; i++) {
-        let x: string = "";
-        let y: string = "";
-        for (let j: number = 0; j < width; j++) {
-            x += " ";
-            y += "#";
-            [x, y] = [y, x];
+    console.log("6 e) - Custom Checkerboard:");
+    function checkerboardcustom(height: number, width: number): void {
+        let output: string = "";
+        for (let i: number = 0; i < height / 2; i++) {
+            let x: string = "";
+            let y: string = "";
+            for (let j: number = 0; j < width; j++) {
+                x += " ";
+                y += "#";
+                [x, y] = [y, x];
+            }
+            output += x + "\n" + y + "\n";
         }
-        output += x + "\n" + y + "\n";
+        console.log(output);
     }
-    console.log(output);
+    checkerboardcustom(4, 20);
+
 }
-checkerboardcustom(4, 20);
