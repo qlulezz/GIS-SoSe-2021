3237c0f50a4031ff476021deea3f8ba240c53609
Henry <HenryCraft@gmx.de>
2021-05-27 18:22:16 +0200

    Added textarea for outputs

--- 3_Server-und-Daten/Aufgabe_3.1/client/submit.js
+++ 3_Server-und-Daten/Aufgabe_3.1/client/submit.js
@@ -5,7 +5,7 @@ var P3_1;
     let submit = document.getElementById("send");
     submit.addEventListener("click", clickSubmit);
     function clickSubmit(_event) {
-        communicate("https://gis-example.herokuapp.com");
+        communicate("https://qlu.herokuapp.com/");
     }
     async function communicate(_url) {
         formData = new FormData(document.forms[0]);
@@ -14,12 +14,26 @@ var P3_1;
         let response = await fetch(_url, { method: "get" });
         let responseString = await response.text();
         console.log(responseString);
+        let footer = document.getElementsByTagName("footer")[0];
+        document.getElementsByTagName("h3")[0].innerHTML = "Ausgabe des Servers:";
+        let textfield;
+        if (document.getElementsByTagName("textarea")[1] == undefined) {
+            textfield = document.createElement("textarea");
+            footer.appendChild(textfield);
+        }
+        else {
+            textfield = document.getElementsByTagName("textarea")[1];
+        }
+        textfield.setAttribute("cols", "80");
+        textfield.setAttribute("rows", "20");
+        textfield.innerHTML = "URL:\n" + _url + "\n\n";
         let output = "\n";
         for (let entry of query) {
-            output += "Entry: " + entry[0] + "\nWert: " + entry[1] + "\n\n";
+            output += "Name: " + entry[0] + "\nValue: " + entry[1] + "\n\n";
             console.log(entry);
         }
         console.log(output);
+        textfield.innerHTML += output;
     }
 })(P3_1 || (P3_1 = {}));
 //# sourceMappingURL=submit.js.map
--- 3_Server-und-Daten/Aufgabe_3.1/client/submit.ts
+++ 3_Server-und-Daten/Aufgabe_3.1/client/submit.ts
@@ -1,10 +1,12 @@
 namespace P3_1 {
+    // Erstelle FormData und finden den Button im HTML
     let formData: FormData;
     let submit: HTMLButtonElement = <HTMLButtonElement>document.getElementById("send");
     submit.addEventListener("click", clickSubmit);
 
+    // Bei Button-Klick, Führe kommunikation mit Server aus
     function clickSubmit(_event: Event): void {
-        communicate("https://gis-example.herokuapp.com");
+        communicate("https://qlu.herokuapp.com/");
     }
 
     async function communicate(_url: RequestInfo): Promise<void> {
@@ -16,11 +18,27 @@ namespace P3_1 {
         let responseString: string = await response.text();
         console.log(responseString);
 
+        // Output in ein Textfeld und die Konsole
+        let footer: HTMLElement = document.getElementsByTagName("footer")[0];
+        document.getElementsByTagName("h3")[0].innerHTML = "Ausgabe des Servers:";
+
+        let textfield: HTMLTextAreaElement;
+        if (document.getElementsByTagName("textarea")[1] == undefined) {
+            textfield = document.createElement("textarea");
+            footer.appendChild(textfield);
+        } else {
+            textfield = document.getElementsByTagName("textarea")[1];
+        }
+        textfield.setAttribute("cols", "80");
+        textfield.setAttribute("rows", "20");
+        textfield.innerHTML = "URL:\n" + _url + "\n\n";
+
         let output: string = "\n";
         for (let entry of query) {
-            output += "Entry: " + entry[0] + "\nWert: " + entry[1] + "\n\n"; 
+            output += "Name: " + entry[0] + "\nValue: " + entry[1] + "\n\n"; 
             console.log(entry);
         }
         console.log(output);
+        textfield.innerHTML += output;
     }
 }
